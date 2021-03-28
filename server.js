393e02594448797f7d04624f63cc1e919a1a2f97
ddyfedd <dzsamboki.techwriter@gmail.com>
2021-03-28 20:02:48 +0200

    first lesson

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const pug = require('pug');
 
 const app = express();
 
@@ -11,11 +12,15 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set('view engine', 'pug');
+
 app.route('/').get((req, res) => {
-  res.render('Load your view here');
+  res.render('views/pug');
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
+
+
