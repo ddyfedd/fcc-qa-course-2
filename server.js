84cbbe12e838e5f30d7b2d2555c9612d03982511
ddyfedd <dzsamboki.techwriter@gmail.com>
2021-03-28 20:06:08 +0200

    correct server.js

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-const pug = require('pug');
 
 const app = express();
+app.set('view engine', 'pug');
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.set('view engine', 'pug');
+
 
 app.route('/').get((req, res) => {
-  res.render('views/pug');
+  res.render(process.cwd() + '/views/pug');
 });
 
 const PORT = process.env.PORT || 3000;
