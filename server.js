2e7035e361daf6c7406abda6e97acb63a54e5459
Dávid Zsámboki <andrazsenator@gmail.com>
2021-03-29 20:31:15 +0000

    copied, corrected hopefully

--- server.js
+++ server.js
@@ -5,13 +5,12 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
-//const { ObjectID } = require('bson');
 const ObjectID = require('mongodb').ObjectID;
 
 const app = express();
 app.set('view engine', 'pug');
 
-fccTesting(app); //For FCC testing purposes
+fccTesting(app); // For fCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -26,37 +25,35 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-myDB(async client => {
+myDB(async (client) => {
   const myDataBase = await client.db('database').collection('users');
 
+  // Be sure to change the title
   app.route('/').get((req, res) => {
-    res.render(process.cwd + '/views/pug/index', { title: 'Connected to DB', message: 'Please login' });
+    // Change the response to render the Pug template
+    res.render('/views/pug', {
+      title: 'Hello',
+      message: 'Please login'
+    });
   });
-  
+
+  // Serialization and deserialization here...
   passport.serializeUser((user, done) => {
     done(null, user._id);
   });
-  
   passport.deserializeUser((id, done) => {
     myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
       done(null, doc);
     });
   });
-
+  // Be sure to add this...
 }).catch((e) => {
   app.route('/').get((req, res) => {
     res.render('pug', { title: e, message: 'Unable to login' });
   });
 });
+// app.listen out here...
 
-//app.route('/').get((req, res) => {
-//  res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
-//});
-
-
-
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Listening on port ' + process.env.PORT);
 });
