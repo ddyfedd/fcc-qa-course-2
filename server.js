c972b85368f8def185d210fb2eac9ead8f79d42c
ddyfedd <dzsamboki.techwriter@gmail.com>
2021-03-29 21:07:30 +0200

    comment out unneeded stuff

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+  //myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
     done(null, null);
-  });
+  //});
 });
 
 const PORT = process.env.PORT || 3000;
