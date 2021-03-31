b893277ab9f468d01bb571c1c2c53e107e102c08
ddyfedd <dzsamboki.techwriter@gmail.com>
2021-03-31 20:27:52 +0200

    authentication added

--- auth.js
+++ auth.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const LocalStrategy = require('passport-local');
 const passport = require('passport');
 const bcrypt = require('bcrypt');
 const ObjectID = require('mongodb').ObjectID;
+const GitHubStrategy = require ('passport-github').Strategy;
 
 module.exports = function(app, myDataBase) {
 
@@ -27,6 +29,16 @@ module.exports = function(app, myDataBase) {
             }
             return done(null, user);
           });
+        }
+      ));
+
+      passport.use(new GitHubStrategy({ 
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: 'https://fcc-qa-course-2.dvidzsmboki.repl.co/auth/github/callback'
+      },
+        function(accessToken, refreshToken, profile, cb) {
+          console.log(profile);
         }
       ));
     
