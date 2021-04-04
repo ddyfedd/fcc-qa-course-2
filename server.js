6bbe618f1ec423ce87e81116d13079f43bed385d
ddyfedd <dzsamboki.techwriter@gmail.com>
2021-04-04 19:13:50 +0200

    last task

--- server.js
+++ server.js
@@ -63,6 +63,13 @@ myDB(async (client) => {
     });
     console.log('user ' + socket.request.user.username + ' connected');
 
+    socket.on('chat message', (message) => {
+      io.emit('chat message', {
+        name: socket.request.user.name,
+        message
+      });
+    });
+
     socket.on('disconnect', () => {
       console.log('A user has disconnected');
       --currentUsers;
